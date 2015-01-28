5316e200b87cf4922d4142def33ca9be5bda9d18
Tudor Marcu <tudormarcu4@gmail.com>
2015-01-28 10:52:15 -0800

    Update parsing for obj model
    Signed-off-by: Tudor Marcu <tudormarcu4@gmail.com>

--- modeload/objload.cpp
+++ modeload/objload.cpp
@@ -24,14 +24,61 @@ static void exerr(string msg)
 	cout << msg << endl;
 }
 
+static void getvertex(char *c, char *n, FILE *f, vector<struct vec3> *vertex)
+{
+	struct vec3 vert;
+
+	if (fscanf(f, "%f %f %f", &vert.x, &vert.y, &vert.z) <= 0)
+		exerr("failed to scan vertices!");
+	vertex->push_back(vert);
+}
+
+static void getnormal(char *c, char *n, FILE *f, vector<struct vec3> *normal)
+{
+	struct vec3 vert;
+
+	if (fscanf(f, "%f %f %f", &vert.x, &vert.y, &vert.z) <= 0)
+		exerr("failed to scan normals!");
+	normal->push_back(vert);
+}
+
+static void getface(char *c, char *n, FILE *f, vector<struct face> *face)
+{
+	struct face temp;
+
+	*c= fgetc(f);
+	*c= fgetc(f);
+	*n= fgetc(f);
+	/* Check for the vertex//normal case */
+	if (*n == '/') {
+		temp.vertindex[0] = atoi(c);
+		*n = fgetc(f);
+		if (*n == '/') {
+			if (fscanf(f, "%i %i//%i %i//%i",
+				&temp.normindex[0],
+				&temp.vertindex[1], &temp.normindex[1],
+				&temp.vertindex[2], & temp.normindex[2]) <= 0)
+					exerr("Failed to read faces!");
+		} else { 
+			exerr(" VERTEX/TEXTURE FORMAT ");
+			exit(EXIT_FAILURE);
+		}
+	} else if (*n == ' ') {
+		/* Handle f v v v format */
+		if (fscanf(f, "%i %i %i", &temp.vertindex[0], &temp.vertindex[1], &temp.vertindex[2]) <= 0)
+			exerr("Failed to read faces! - expecting: f v v v");
+	}
+
+
+	face->push_back(temp);
+}
+
 int main(int argc, char *argv[])
 {
 	string line;
 	FILE *file;
-	char c = 0;
+	char curr = 0;
 	char next = 0;
-	struct vec3 vertex;
-	struct face temp;
 	vector<struct vec3> vertices;
 	vector<struct vec3> normals;
 	vector<struct face> faces;
@@ -43,48 +90,33 @@ int main(int argc, char *argv[])
 	}
 
 	/* Read in data */
-	c = fgetc(file);
-	next = c;
+	curr = fgetc(file);
+	next = curr;
 	next = fgetc(file);
-	while (c != EOF) {
-		if (c == 'v' && next == ' ') {
-			if (fscanf(file, "%f %f %f", &vertex.x, &vertex.y, &vertex.z) <= 0)
-				exerr("failed to scan vertices!");
-			vertices.push_back(vertex);
-		} else if (c == 'v' && next == 'n') {
-			if (fscanf(file, "%f %f %f", &vertex.x, &vertex.y, &vertex.z) <= 0)
-				exerr("failed to scan normals!");
-			normals.push_back(vertex);
-		} else if (c == 'f') {
-			c = fgetc(file);
-			c = fgetc(file);
-			next = fgetc(file);
-			/* Check for the vertex//normal case */
-			if (next == '/') {
-				temp.vertindex[0] = atoi(&c);
-				next = fgetc(file);
-				if (next == '/') {
-					if (fscanf(file, "%i %i//%i %i//%i",
-						&temp.normindex[0],
-						&temp.vertindex[1], &temp.normindex[1],
-						&temp.vertindex[2], & temp.normindex[2]) <= 0)
-							exerr("Failed to read faces!");
-					}
-			}
-			faces.push_back(temp);
+	while (curr != EOF) {
+		if (curr == 'v' && next == ' ') {
+			getvertex(&curr, &next, file, &vertices);
+		} else if (curr == 'v' && next == 'n') {
+			getnormal(&curr, &next, file, &normals);
+		} else if (curr == 'f') {
+			getface(&curr, &next, file, &faces);
 		}
-		c = next;
+		curr = next;
 		next = fgetc(file);
 	}
 	cout << vertices.size() << " vertices\n" <<
 			normals.size() << " normals\n" <<
-			faces.size() << " faces" << endl;
+			faces.size() << " Triangles (faces)" << endl;
 	for (size_t i = 0; i < vertices.size(); i++)
 		cout << vertices[i].x << " "<< vertices[i].y << " " << vertices[i].z << endl;
-
 	for (size_t i = 0; i < normals.size(); i++)
 		cout << normals[i].x << " " << normals[i].y << " " << normals[i].z << endl;
 	for (size_t i = 0; i < faces.size(); i++)
 		cout << faces[i].vertindex[0] << "//" << faces[i].normindex[0] << endl;
+	for (size_t i = 0; i < faces.size(); i++)
+		cout << faces[i].vertindex[0] << " " << faces[i].vertindex[1] << " " << faces[i].vertindex[2] << endl;
+
+	fclose(file);
+
 	return 0;
 }
